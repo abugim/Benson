66c333941b4dc01f29006487b33d4a1d794fed74
Yuri Thomas <yuri.tpinheiro@gmail.com>
2016-01-31 10:39:48 -0300

    Tsunami Beta 2

--- classes/tsunami.h
+++ classes/tsunami.h
@@ -13,18 +13,61 @@ class tsunami
 public:
 	tsunami();
 	~tsunami();
-private:
-	double valor();
 	
+	double valor() {
+
+		if (tempo > periodo){
+			tempo = 0;
+		} else { 
+			tempo += 0.1;
+		}
+
+		switch(tipo)
+		{
+			case DEGRAU:
+				return this->amp + this->offset;
+				break;
+
+			case SENOIDAL:
+				return amp * sin((tempo / periodo) * 2 * M_PI) + this->offset;
+				break;
+
+			case QUADRADA:
+				if (tempo < periodo/2.f)
+					return this->amp + this->offset;
+				else
+					return -this->amp + this->offset;
+				break;
+
+			case SERRA:
+				return (2 * amp / periodo) * tempo - amp + this->offset;
+				break;
+
+			case ALEATORIO:
+				if (tempo == 0) {
+					srand (time(NULL));	
+					periodo = ((float) (rand()) / (float) (RAND_MAX)) * (periodo_sup - periodo_inf) + periodo_inf;
+					amp = ((float) (rand()) / (float) (RAND_MAX)) * (amp_sup - amp_inf) + amp_inf;
+				}
+				return this->amp;
+				break;
+		}
+	}
+
+	// Gets and sets
+
+private:
+	int tipo;
+
 	double tempo;
 
 	double amp;
 	double amp_sup;
 	double amp_inf;
 
-	double period;
-	double period_sup;
-	double period_inf;
+	double periodo;
+	double periodo_sup;
+	double periodo_inf;
 
 	double offset;
 };
